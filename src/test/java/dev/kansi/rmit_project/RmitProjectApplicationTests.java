3a690b6bb6a98c35c8af14f07a109abf65cc92a7
kaniser1 <kansikrister@gmail.com>
2024-08-04 15:46:23 +0300

    Initial commit

---
+++ src/test/java/dev/kansi/rmit_project/RmitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kansi.rmit_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
